27ef10f3a2d60748bbc7f1782def701c20d86b83
skplanet-tech <green2902@gmail.com>
2023-02-20 16:12:50 +0900

    footer 문구 변경 및 포스팅 날짜 수정

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Footer = ({ onChange }) => (
   <footer className="footer" onClick={onChange}>
-    ©{` ${new Date().getFullYear()}`} SK Planet
+    COPYRIGHT © {`${new Date().getFullYear()}`} SK Planet. ALL RIGHT RESERVED.
   </footer>
 );
 
